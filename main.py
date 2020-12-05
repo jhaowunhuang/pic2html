372350e2ba87ed9f064f6cb71bf965933b2b8ba7
jhaowunhuang <jhaowunhuang@gmail.com>
2020-12-06 04:18:48 +0800

    workable version

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ def rgb2hex(rgb_arr):
 def pic2html(input_image, output_html):
     img = cv2.imread(input_image)
     h, w, _ = img.shape
-    h //= 10
-    w //= 10
+    h //= 50
+    w //= 50
+    print(h, w)
     img = cv2.resize(img, (h, w), interpolation = cv2.INTER_AREA) 
     cvt_img = map(lambda arr: map(rgb2hex, arr), img)
     content = '<html><head></head><body>'
-    content += '<table style=\"font-size:0px; height: ' + str(h) + 'px; width: ' + str(w) + 'px\" cellspacing=0 cellpadding=0 border=0>'
+    content += '<table height=' + str(h) + ' width=' + str(w) + ' cellspacing=0 cellpadding=0 border=0>'
     for i, row in enumerate(cvt_img):
         content += '<tr>'
         for j, elem in enumerate(row):
-            content += '<td bgcolor=' + elem + '>&nbsp;</td>'
+            content += '<td style=\"font-size:0px;\"  bgcolor=' + elem + '><!></td>'
         content += '</tr>'
     content += '</table></body></html>'
     with open(output_html, 'w') as f_out:
