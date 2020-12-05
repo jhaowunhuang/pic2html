d6fb168dad50e545c2e6dc8d104cb42da2b8a9db
jhaowunhuang <jhaowunhuang@gmail.com>
2020-12-06 03:47:38 +0800

    simplified code

--- main.py
+++ main.py
@@ -2,30 +2,26 @@ import cv2
 
 
 def rgb2hex(rgb_arr):
-    return '#%02x%02x%02x' % tuple(rgb_arr)
+    return '#%02x%02x%02x' % tuple(rgb_arr[::-1])
     
 
 def pic2html(input_image, output_html):
     img = cv2.imread(input_image)
-    w, h, _ = img.shape
+    h, w, _ = img.shape
+    h //= 10
+    w //= 10
+    img = cv2.resize(img, (h, w), interpolation = cv2.INTER_AREA) 
     cvt_img = map(lambda arr: map(rgb2hex, arr), img)
-    content = ''
+    content = '<html><head></head><body>'
+    content += '<table style=\"font-size:0px; height: ' + str(h) + 'px; width: ' + str(w) + 'px\" cellspacing=0 cellpadding=0 border=0>'
     for i, row in enumerate(cvt_img):
-        if i >= 100:
-            continue
         content += '<tr>'
         for j, elem in enumerate(row):
-            if j >= 100:
-                continue
             content += '<td bgcolor=' + elem + '>&nbsp;</td>'
         content += '</tr>'
-    html_begin = '<html><head><style type=\"text/css\">table{table-layout:fixed;empty-cells:hide;height:100px;width:100px;border-top:0px;margin:0px;padding:0px;font-size:0px}</style></head><body><table cellspacing=0 cellpadding=0 border=0>'
-    html_end = '</table></body></html>'
+    content += '</table></body></html>'
     with open(output_html, 'w') as f_out:
-        f_out.write(html_begin)
         f_out.write(content)
-        f_out.write(html_end)
-        
 
 
 pic2html('cat.jpg', 'out.html')
